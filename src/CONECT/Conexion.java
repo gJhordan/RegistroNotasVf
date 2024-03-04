4d887cc49d4dff9e8488766d6dadc15f6adccdf3
Administrador <Administrador@DESKTOP-O9BQD7L>
2024-03-03 23:10:57 -0500

    hola

--- src/CONECT/Conexion.java
+++ src/CONECT/Conexion.java
@@ -26,7 +26,7 @@ public class Conexion {
 //                System.out.println("CONECTADO");
             }
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("No se conectó " + e);
+            System.out.println("No se conectó porque:" + e);
         }
         return con;
     }
